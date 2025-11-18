b6b3afb6364fd129f0ddb5f673d77235c70a3ff8
Hugo Alvarado <haleas03@gmail.com>
2025-11-18 14:29:04 -0600

    feat: animation is added when you update a note

--- src/NotesList.jsx
+++ src/NotesList.jsx
@@ -19,6 +19,7 @@ export default function NotesList({ notes, onSelect, selectedNoteId, onCreate, o
         <div
           key={note.id}
           onClick={() => onSelect(note.id)}
+          className="note-item"
           style={{
             padding: 10,
             marginBottom: 10,
